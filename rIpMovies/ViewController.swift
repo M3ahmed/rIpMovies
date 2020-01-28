8ea727eedf9f114054d3e43645aea6cb58cad0fb
Mohamed Ahmed <m3ahmed@ucsd.edu>
2020-01-28 14:29:09 -0800

    First commit

---
+++ rIpMovies/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  rIpMovies
+//
+//  Created by Mohamed S Ahmed on 1/28/20.
+//  Copyright © 2020 Mohamed S Ahmed. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
