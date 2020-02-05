81e372ff2205b9d6e6581ba87a4798c2d9558fe4
Mohamed Ahmed <m3ahmed@ucsd.edu>
2020-02-04 17:07:37 -0800

    Had to leave my computer

---
+++ rIpMovies/MovieDetailsViewController.swift
@@ -0,0 +1,54 @@
+//
+//  MovieDetailsViewController.swift
+//  rIpMovies
+//
+//  Created by Mohamed S Ahmed on 2/4/20.
+//  Copyright © 2020 Mohamed S Ahmed. All rights reserved.
+//
+
+import UIKit
+import AlamofireImage
+
+class MovieDetailsViewController: UIViewController {
+
+    @IBOutlet weak var backDropView: UIImageView!
+    @IBOutlet weak var posterView: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var synopsisLabel: UILabel!
+    
+    var movie: [String: Any]!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        
+        titleLabel.text = movie["title"] as? String
+        synopsisLabel.text = movie["overview"] as? String
+        synopsisLabel.sizeToFit()
+        
+        let baseURL = "https://image.tmdb.org/t/p/w185"
+        let posterPath = movie["poster_path"] as! String
+        let posterUrl = URL(string: baseURL + posterPath)
+              
+        posterView.af_setImage(withURL: posterUrl!)
+        
+        let backDropPath = movie["backdrop_path"] as! String
+        let backDropUrl = URL(string: "https://image.tmdb.org/t/p/w780" + backDropPath)
+              
+        backDropView.af_setImage(withURL: backDropUrl!)
+        
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
